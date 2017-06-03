1c3590b983f13d197675ed9378ad54cc27ade943
Scott Chow <scottpchow@umail.ucsb.edu>
2017-06-02 17:40:06 -0700

    Challenge 1

--- Swift Ninja Challenge.playground/Contents.swift
+++ Swift Ninja Challenge.playground/Contents.swift
@@ -2,4 +2,18 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Challenge 1: Swap two variables in one line of code
+
+
+func swap<T>( a: inout T,  with b: inout T) {
+    (a, b) = (b, a)
+}
+var a = "lmao"
+var b = "ayy"
+
+swap(&a, &b)
+
+print(a)
+print(b)
+
+// Challenge 2: 
