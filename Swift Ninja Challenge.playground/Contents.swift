89db447df1d2ce50c368b93d241063f4f249c752
Scott Chow <scottpchow@umail.ucsb.edu>
2017-06-04 14:21:34 -0700

    Challenge 2

--- Swift Ninja Challenge.playground/Contents.swift
+++ Swift Ninja Challenge.playground/Contents.swift
@@ -2,6 +2,9 @@
 
 import UIKit
 
+
+var array = [Double](repeatElement(0.0, count: 3))
+
 // Challenge 1: Swap two variables in one line of code
 
 
@@ -16,4 +19,14 @@ swap(&a, &b)
 print(a)
 print(b)
 
-// Challenge 2: 
+// Challenge 2: Have a single function be able to take a variable number of parameters
+
+func flexString (string1: String = "", string2: String = "") -> String {
+    return "First string is: " + string1 + " and the second string is: " + string2;
+}
+
+print(flexString())
+print(flexString(string1: "hey"))
+print(flexString(string1: "hey", string2: "you"))
+
+// Challenge 3: 
